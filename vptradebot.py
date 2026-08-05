2f645ab3fabc5e7bfc4f06398d66e2837321cf57
ksj1388 <ksj1388@users.noreply.github.com>
2026-08-05 08:18:48 +0330

    Fix fullscreen toggle: use show/hide instead of splitter sizes

--- vptradebot.py
+++ vptradebot.py
@@ -5722,14 +5722,24 @@ class ChartWidget(QWidget):
         self._is_fullscreen = not self._is_fullscreen
         ms = mw.middle_splitter
         vs = mw.main_splitter
+        sp = mw.strategy_panel
+        wp = mw.watchlist_panel
+        bt = mw.bottom_tabs
         if self._is_fullscreen:
             self._saved_sizes = ms.sizes()
             self._saved_vsize = vs.sizes()
-            ms.setSizes([0, 1, 0])
-            vs.setSizes([1, 0])
+            self._saved_sp = sp.isVisible()
+            self._saved_wp = wp.isVisible()
+            self._saved_bt = bt.isVisible()
+            sp.hide()
+            wp.hide()
+            bt.hide()
             self._fullscreen_btn.setText("⛶")
             self._fullscreen_btn.setToolTip("Exit Fullscreen")
         else:
+            sp.setVisible(self._saved_sp)
+            wp.setVisible(self._saved_wp)
+            bt.setVisible(self._saved_bt)
             if hasattr(self, '_saved_sizes'):
                 ms.setSizes(self._saved_sizes)
             if hasattr(self, '_saved_vsize'):
